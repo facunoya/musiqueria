7e3a8d9850124eebac1b145fb7fcd9a8bcf79c72
Facundo Noya <facunoya@hotmail.com>
2024-03-19 20:04:12 -0300

    production config

--- database/config/config.js
+++ database/config/config.js
@@ -7,10 +7,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "u9kyvmdsd0algvdx",
-    "password": "5XDFvwNWdapX5q0ZcVKj",
-    "database": "bnfx7o3xabyageii8uze",
-    "host": "bnfx7o3xabyageii8uze-mysql.services.clever-cloud.com",
+    "username": "root",
+    "password": null,
+    "database": "musiqueria",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
